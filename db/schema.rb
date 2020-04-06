0817cd6785a71ffd507f8ff12abcb87b5c949ae4
Tom Harwood <tharwood26@gmail.com>
2020-04-06 16:41:32 +0100

    Add pizza model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_150756) do
+ActiveRecord::Schema.define(version: 2020_04_06_154017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pizzas", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
