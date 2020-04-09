a058c2ef6b89b6a29076a85acaf38c808d0f004b
Tom Harwood <tharwood26@gmail.com>
2020-04-09 16:41:07 +0100

    JS navbar added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require cloudinary
+// require cloudinary
+import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,6 +18,9 @@ require cloudinary
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+document.addEventListener('turbolinks:load', () => {
+  initUpdateNavbarOnScroll();
+});
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
@@ -23,12 +28,11 @@ require cloudinary
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
+// document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
+// });
