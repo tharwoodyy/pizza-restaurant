6a53ad2b27955d355f18f5dc2e70d9b053c67a67
Tom Harwood <tharwood26@gmail.com>
2020-04-14 10:03:03 +0100

    orders controller done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,24 +10,24 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   @car = Car.find(params[:car_id])
-  #   @booking.car = @car
-  #   @booking.user = current_user
-  #   redirect_to car_path(@car) if @car.user == current_user
-  #   if @booking.save!
-  #     redirect_to car_bookings_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @order = Order.new(order_params)
+    @pizza = Pizza.find(params[:pizza_id])
+    @order.pizza = @pizza
+    @order.user = current_user
+    redirect_to pizza_path(@pizza) if @pizza.user == current_user
+    if @order.save!
+      redirect_to pizza_orders_path
+    else
+      render :new
+    end
+  end
 
-  # def destroy
-  #   @booking = Booking.find(params[:id])
-  #   @booking.destroy
-  #   redirect_to bookings_path
-  # end
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to orders_path
+  end
 
   private
 
